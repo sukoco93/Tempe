353183bf8043f8ade0cb758a15f644577838235a
sukoco93 <82564991+sukoco93@users.noreply.github.com>
2026-07-09 21:48:43 +0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,29 +1,9 @@
 console.log('Vue version:', Vue.version);
-import { dataMixin } from './mixins/dataMixin.js';
-import { computedMixin } from './mixins/computedMixin.js';
-import { crudMixin } from './mixins/crudMixin.js';
-import { uiMixin } from './mixins/uiMixin.js';
-import { exportImportMixin } from './mixins/exportImportMixin.js';
 
-// ... import mixins
 new Vue({
     el: '#app',
-    mixins: [
-        dataMixin,
-        computedMixin,
-        crudMixin,
-        uiMixin,
-        exportImportMixin
-    ],
-    mounted() {
-        console.log('Vue mounted!');
-        this.loadData();
-
-        // Registrasi Service Worker
-        if ('serviceWorker' in navigator) {
-            navigator.serviceWorker.register('/sw.js')
-                .then(reg => console.log('SW registered'))
-                .catch(err => console.error('SW registration failed', err));
-        }
-    }
+    data() {
+        return { message: 'Halo Tempe! Aplikasi berjalan.' };
+    },
+    template: `<div style="padding:20px;font-size:24px;color:#00796b;">{{ message }}</div>`
 });
