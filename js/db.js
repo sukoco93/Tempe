4531c05cc4cb6ef732a7f0319d37b23298e2d9e8
sukoco93 <82564991+sukoco93@users.noreply.github.com>
2026-07-09 22:52:18 +0700

    Update db.js

--- js/db.js
+++ js/db.js
@@ -1,8 +1,5 @@
-// Inisialisasi Dexie
-import Dexie from 'https://unpkg.com/dexie@4.0.8/dist/dexie.js'; // atau bisa pakai window.Dexie
-
-export const db = new Dexie("TokoAndroidDB_TopSheet_FinalFix");
-db.version(1).stores({
+window.db = new Dexie("TokoAndroidDB_TopSheet_FinalFix");
+window.db.version(1).stores({
     pelanggan: '++id, nama, hp',
     penjualan: '++id, tgl, pelangganId, barang, total',
     kas: '++id, tgl, jenis, keterangan, nominal, isAutomated, pelangganId',
